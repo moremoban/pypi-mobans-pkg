5f670b1c775f1f8b11c5ccf2f815137c2c2d641a
chfw <wangc_2011@hotmail.com>
2019-05-04 20:37:20 +0100

    :egg: :ferris_wheel: release 0.0.4

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 pip freeze
-nosetests --with-coverage --cover-package pypi_mobans_pkg --cover-package tests tests  docs/source pypi_mobans_pkg && flake8 . --exclude=.moban.d,docs --builtins=unicode,xrange,long
+nosetests --with-coverage --cover-package pypi_mobans_pkg --cover-package tests tests  docs/source pypi_mobans_pkg
