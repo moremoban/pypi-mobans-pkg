c54340995df2b071fe8486c3594fd02996e5cc97
chfw <wangc_2011@hotmail.com>
2020-01-14 22:51:23 +0000

    :egg: release 0.0.10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ project = 'pypi-mobans-pkg'
 copyright = '2018-2020 Onni Software Ltd. and its contributors'
 author = 'C.W.'
 # The short X.Y version
-version = '0.0.9'
+version = '0.0.10'
 # The full version, including alpha/beta/rc tags
-release = '0.0.9'
+release = '0.0.10'
 
 # -- General configuration ---------------------------------------------------
 
