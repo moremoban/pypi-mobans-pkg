1aa41031eed93685d0ce82ca48b3b820cd719a65
chfw <chfw@users.noreply.github.com>
2020-09-13 11:55:28 +0000

    This is an auto-commit, updating project meta data, such as changelog.rst, contributors.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ project = 'pypi-mobans-pkg'
 copyright = '2018-2020 Onni Software Ltd. and its contributors'
 author = 'C.W.'
 # The short X.Y version
-version = '0.1.1'
+version = '0.1.2'
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 
